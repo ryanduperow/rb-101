05dea2d37ea8f02056949f99398082d368691591
Ryan DuPerow <rkdupe@gmail.com>
2021-01-29 13:27:45 +0000

    lesson 4 exercises

---
+++ lesson_4/loops1_10.rb
@@ -0,0 +1,7 @@
+# Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends do
+  puts "Hello, #{friend}!"
+end
