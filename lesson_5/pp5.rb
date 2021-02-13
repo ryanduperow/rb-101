f5417f30be666ffe34ad7cef002b3c50892d8841
Ryan DuPerow <rkdupe@gmail.com>
2021-02-13 16:55:25 +0000

    Exercises for 2/13/21

---
+++ lesson_5/pp5.rb
@@ -0,0 +1,16 @@
+# Given this nested hash, figure out the total age of just the male members of the family.
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+total_male_age = 0
+munsters.each_value do |details|
+  total_male_age += details["age"] if details["gender"] == "male"
+end
+
+p total_male_age
