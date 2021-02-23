9befbf4d2f7525af27137d74053d7fd6d645d0c0
Ryan DuPerow <rkdupe@gmail.com>
2021-02-23 09:02:38 -0500

    edited tic tac toe game with rubocop 2/23/21

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +  # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +  # columns
+                [[1, 5, 9], [3, 5, 7]]               # diagonals
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
@@ -5,8 +10,9 @@ COMPUTER_MARKER = 'O'
 
 def prompt(msg)
   puts "=> #{msg}"
-end  
+end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts "You're #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
@@ -24,15 +30,16 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num]  == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
@@ -41,11 +48,11 @@ def player_places_piece!(brd)
     prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid choice." 
+    prompt "Sorry, that's not a valid choice."
   end
 
   brd[square] = PLAYER_MARKER
-end  
+end
 
 def computer_places_piece!(brd)
   square = empty_squares(brd).sample
@@ -58,26 +65,18 @@ end
 
 def someone_won?(brd)
   !!detect_winner(brd)
-end  
+end
 
 def detect_winner(brd)
-  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +  # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +  # columns
-                  [[1, 5, 9], [3, 5, 7]]               # diagonals
-
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-       brd[line[1]] == COMPUTER_MARKER &&
-       brd[line[2]] == COMPUTER_MARKER
-       return 'Computer'  
-    end  
-  end 
-  nil 
-end  
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
 
 loop do
   board = initialize_board
@@ -95,7 +94,7 @@ loop do
   display_board(board)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!" 
+    prompt "#{detect_winner(board)} won!"
   else
     prompt "It's a tie!"
   end
