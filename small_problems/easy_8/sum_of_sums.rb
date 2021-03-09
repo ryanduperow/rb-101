6ba620fd206f8bac43ae5cc71a15e39e7cb794f8
Ryan DuPerow <rkdupe@gmail.com>
2021-03-09 10:12:10 -0500

    Exercise completed on 3/9/21

---
+++ small_problems/easy_8/sum_of_sums.rb
@@ -0,0 +1,20 @@
+# Write a method that takes an Array of numbers and then returns the sum of the
+# sums of each leading subsequence for that Array. You may assume that the Array
+# always contains at least one number.
+
+def sum_of_sums(numbers)
+  sum_total = 0
+  accumulator = 0
+
+  numbers.each do |num|
+    accumulator += num
+    sum_total += accumulator
+  end
+
+  sum_total
+end
+
+p sum_of_sums([3, 5, 2])
+p sum_of_sums([1, 5, 7, 3])
+p sum_of_sums([4])
+p sum_of_sums([1, 2, 3, 4, 5])
